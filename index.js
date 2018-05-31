598120d7a1200fdbbc476a4f8af2245c4f41d17e
aricallen <aric.allen2@gmail.com>
2018-05-30 20:57:47 -0700

    add git and yarn execs

--- index.js
+++ index.js
@@ -7,21 +7,48 @@
 
 const fs = require('fs');
 const path = require('path');
+const shelljs = require('shelljs');
+const { promisify } = require('util');
 const recursiveReadDirSync = require('recursive-readdir-sync');
 const changeCase = require('change-case');
 
-const templates = recursiveReadDirSync(path.resolve(__dirname, './templates'));
+const templateDir = 'templates';
+const templates = recursiveReadDirSync(path.resolve(__dirname, `./${templateDir}`));
 
-const moduleName = process.argv[2];
+const moduleName = changeCase.paramCase(process.argv[2]);
 const moduleNamePascal = changeCase.pascalCase(moduleName);
+const moduleDir = path.join(process.cwd(), moduleName);
+
+// create module dir
+if (fs.existsSync(moduleDir) === false) {
+  fs.mkdirSync(moduleDir);
+}
+// create vscode dir
+if (fs.existsSync(path.join(moduleDir, '.vscode')) === false) {
+  fs.mkdirSync(path.join(moduleDir, '.vscode'));
+}
 
 for (const filePath of templates) {
   const content = fs.readFileSync(filePath, { encoding: 'utf8' });
   const scrubbed = content
     .replace('{{moduleName}}', moduleName)
     .replace('{{moduleNamePascal}}', moduleNamePascal);
-  const relativeDest = filePath.replace(__dirname, '');
-  const dest = path.join(process.cwd(), changeCase.paramCase(moduleName), relativeDest);
+  const relativeDest = filePath
+    .replace(__dirname, '')
+    .replace(`/${templateDir}/`, '');
+  const dest = path.join(process.cwd(), moduleName, relativeDest);
   console.log(`writing ${relativeDest} -> ${dest}`);
-  // fs.writeFileSync(dest, scrubbed, { encoding: 'utf8' });
+  fs.writeFileSync(dest, scrubbed, { encoding: 'utf8' });
 }
+
+// cd into the module dir
+const modulePath = path.join(process.cwd(), moduleName);
+shelljs.cd(modulePath);
+
+const exec = promisify(shelljs.exec);
+
+// install packages
+exec('yarn install')
+  .then(() => exec('git init'))
+  .then(() => exec('git add .'))
+  .then(() => exec('git commit -am "initial commit"'));
